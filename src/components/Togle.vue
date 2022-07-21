9e400e7f93ca893028a31b7aed92027bf6a97227
Lee tae hoon <100104142+hoontail@users.noreply.github.com>
2022-07-21 19:33:08 +0900

    Create Togle.vue

---
+++ src/components/Togle.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 v-if="isOpen">토글</h1>
+  <button @click="toggle">{{ isOpen === true ? "OFF" : "ON" }}</button>
+  <div id="hello">dd</div>
+  <input :value="text" @input="onInput" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BoardList",
+  props: {},
+  data: function () {
+    return {
+      text: "",
+      isOpen: true,
+    };
+  },
+  methods: {
+    onInput(e: any) {
+      this.text = e.target.value;
+      console.log(this.text);
+    },
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
